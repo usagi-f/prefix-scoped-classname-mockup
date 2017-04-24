afa7319d3fdf203c8f3b03a70fdc14807e873ba1
usagi.f@gmail.com <usagi.f@gmail.com>
2017-04-24 12:23:18 +0900

    add: stylestats

---
+++ stylestats.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const postcss = require('postcss');
+const stylestats = require('postcss-stylestats');
+const postcssImport = require('postcss-import');
+const postcssCssVariables = require('postcss-css-variables');
+const postcssNested = require('postcss-nested');
+
+const settings = {
+    entry: 'src/components/index.css',
+    output: 'stylestats/index.json',
+    options: {
+        type: 'json',
+    },
+};
+
+fs.readFile(settings.entry, (err, css) => {
+    postcss([postcssImport, postcssCssVariables, postcssNested])
+        .process(css, { from: settings.entry})
+        .then(result => {
+            postcss()
+                .use(stylestats(settings.options))
+                .process(result.css)
+                .then(result => {
+                    if (result.messages[0].stats) {
+                        fs.writeFile(settings.output, result.messages[0].stats, (err) => {
+                            if (err) throw err;
+                            console.log('writeFile Done !');
+                        });
+                    }
+                });
+        });
+});
